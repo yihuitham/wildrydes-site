754a31dfefed29f4d274c05aa88defa05776b68e
Yihui Tham <yihuitham@gmail.com>
2022-05-28 11:33:01 +0800

    update user pool info

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-southeast-1_h3XlQ7ia", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "5l53u0181f3go23m2jk3f344sm", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-southeast-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
